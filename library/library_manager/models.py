df0c4cb73c40bd170d71b35c6348382b29386727
MinjonH <97527244+MinjonH@users.noreply.github.com>
2023-03-18 17:55:51 +0200

    logout

--- library/library_manager/models.py
+++ library/library_manager/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django_extensions.db.fields import AutoSlugField
 
 class Student(models.Model):
     name = models.CharField(max_length=255, blank=False)
     grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(12)])
     book_limit = models.IntegerField(default=3)
-    slug = models.SlugField(max_length=255)
+    slug = AutoSlugField(populate_from='name') 
 
     def __str__(self):
         return self.name
 
 class Series(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='name') 
+    
+    class Meta:
+        verbose_name_plural = 'Series'
+
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255, blank=False)
     author = models.CharField(max_length=255, blank=True)
     series = models.ForeignKey(Series, related_name='book', on_delete=models.CASCADE)   
     is_issued = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=255)
+    slug = AutoSlugField(populate_from='title') 
 
     def __str__(self) -> str:
         return self.title
